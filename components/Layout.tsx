c8a330cba70c0e10ba445e76f9f3e00ac607092c
Shulepov <mikhail.shulepov@gmail.com>
2023-03-05 12:22:13 +0800

    twtiter

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,6 +6,9 @@ const Layout: React.FC<{}> = ({ children }) =>{
       <main className="" id="mint">
           {children}
         </main>
+        <footer className="flex flex-row justify-center w-full py-4">
+          <a href="https://twitter.com/blockside_club" target="_blank" className="text-cyan-600 hover:text-cyan-800">Twitter</a>
+        </footer>
     </div>
   );
 }
